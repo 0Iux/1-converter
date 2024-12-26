8fc28d9949925586c49064de12b7ee30a3874bb8
Student <nsalabugas@gmail.com>
2024-12-26 20:39:20 +0300

    Добавил функцию считывания данных от пользователя, пустую функцию , в которой будет производится расчет курса, так же вынес константы за пределы функций.

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
-	const usd_eur = 0.96
-	const usd_rub = 99.96
-	const eur_rub = usd_rub / usd_eur
+const usd_eur = 0.96
+const usd_rub = 99.96
+const eur_rub = usd_rub / usd_eur
+
+func main() {
+}
+
+func get_user_input() (string, float64, string) {
+	var start_currency, need_currency string
+	var ammount float64
+	fmt.Println("Какая валюта у вас сейчас? ")
+	fmt.Scan((&start_currency))
+	fmt.Println("Сколько у вас этой валюты?: ")
+	fmt.Scan((&ammount))
+	fmt.Println("Какую валюту вы хотите получить? ")
+	fmt.Scan((&need_currency))
+	return start_currency, ammount, need_currency
+}
+
+func calculate_exchange(start_currency, need_currency string, ammount float64) float64 {
 
 }
